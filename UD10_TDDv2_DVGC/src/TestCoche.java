1d3200c2375aff0f1b8d9a3a95924e713561fc39
Valentina Galeote <valentinagaleote@gmail.com>
2023-04-01 20:08:08 +0200

    Refactorizados los nombres de los métodos en TestCoche añadiendo mi nombre al final.

--- UD10_TDDv2_DVGC/src/TestCoche.java
+++ UD10_TDDv2_DVGC/src/TestCoche.java
@@ -4,27 +4,27 @@ import org.junit.jupiter.api.Test;
 class TestCoche {
 
 	@Test
-	void test_crear_coche_su_velocidad_es_cero() {
+	void test_crear_coche_su_velocidad_es_cero_Daniela_Valentina_Galeote_Calderon() {
 		Coche nuevoCoche = new Coche();
 		Assertions.assertEquals(0, nuevoCoche.velocidad);
 	}
 	
 	@Test
-	void test_al_acelerar_un_coche_su_velocidad_aumenta() {
+	void test_al_acelerar_un_coche_su_velocidad_aumenta_Daniela_Valentina_Galeote_Calderon() {
 		Coche nuevoCoche = new Coche();
 		nuevoCoche.acelerar(30);
 		Assertions.assertEquals(30, nuevoCoche.velocidad);
 	}
 	
 	@Test
-	void test_al_decelerar_un_coche_su_velocidad_disminuye() {
+	void test_al_decelerar_un_coche_su_velocidad_disminuye_Daniela_Valentina_Galeote_Calderon() {
 		Coche nuevoCoche = new Coche();
 		nuevoCoche.velocidad = 50;
 		nuevoCoche.decelerar(20);
 		Assertions.assertEquals(30, nuevoCoche.velocidad);
 	}
 	@Test
-	void test_al_decelerar_un_coche_su_velocidad_no_puede_ser_menor_que_cero() {
+	void test_al_decelerar_un_coche_su_velocidad_no_puede_ser_menor_que_cero_Daniela_Valentina_Galeote_Calderon() {
 		Coche nuevoCoche = new Coche();
 		nuevoCoche.velocidad = 50;
 		nuevoCoche.decelerar(80);
