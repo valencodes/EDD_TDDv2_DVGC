ccc64f28fa9c483189bdd501d291eff9b1cc682f
Valentina Galeote <valentinagaleote@gmail.com>
2023-04-01 19:47:31 +0200

    Test al decelerar la velocidad de un coche disminuye

--- UD10_TDDv2_DVGC/src/Coche.java
+++ UD10_TDDv2_DVGC/src/Coche.java
@@ -3,4 +3,12 @@ public class Coche {
 
 	public int velocidad;
 
+	public void acelerar(int aceleracion) {
+		velocidad += aceleracion;
+	}
+
+	public void decelerar(int deceleracion) {
+		velocidad -= deceleracion;
+	}
+
 }
--- UD10_TDDv2_DVGC/src/TestCoche.java
+++ UD10_TDDv2_DVGC/src/TestCoche.java
@@ -1,5 +1,3 @@
-import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -10,5 +8,20 @@ class TestCoche {
 		Coche nuevoCoche = new Coche();
 		Assertions.assertEquals(0, nuevoCoche.velocidad);
 	}
-
+	
+	@Test
+	void test_al_acelerar_un_coche_su_velocidad_aumenta() {
+		Coche nuevoCoche = new Coche();
+		nuevoCoche.acelerar(30);
+		Assertions.assertEquals(30, nuevoCoche.velocidad);
+	}
+	
+	@Test
+	void test_al_decelerar_un_coche_su_velocidad_disminuye() {
+		Coche nuevoCoche = new Coche();
+		nuevoCoche.velocidad = 50;
+		nuevoCoche.decelerar(20);
+		Assertions.assertEquals(30, nuevoCoche.velocidad);
+	}
+	
 }
