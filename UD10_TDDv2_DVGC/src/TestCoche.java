2c4d7c5ad5d92f38b6fcf6abb717823f0b7ed856
Valentina Galeote <valentinagaleote@gmail.com>
2023-04-01 19:23:20 +0200

    TestCoche creado

---
+++ UD10_TDDv2_DVGC/src/TestCoche.java
@@ -0,0 +1,11 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+class TestCoche {
+
+	@Test
+	void test_crear_coche() {
+		Coche nuevoCoche = new Coche();
+	}
+
+}
