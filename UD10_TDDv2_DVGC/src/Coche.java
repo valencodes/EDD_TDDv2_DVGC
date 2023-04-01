8ca6ec62787ecb39570c1393814c89974233b6ff
Valentina Galeote <valentinagaleote@gmail.com>
2023-04-01 19:32:01 +0200

    test que cuando se crea el coche su velocidad sea igual a 0

--- UD10_TDDv2_DVGC/src/Coche.java
+++ UD10_TDDv2_DVGC/src/Coche.java
@@ -1,4 +1,6 @@
 
 public class Coche {
 
+	public int velocidad;
+
 }
--- UD10_TDDv2_DVGC/src/TestCoche.java
+++ UD10_TDDv2_DVGC/src/TestCoche.java
@@ -1,11 +1,14 @@
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class TestCoche {
 
 	@Test
-	void test_crear_coche() {
+	void test_crear_coche_su_velocidad_es_cero() {
 		Coche nuevoCoche = new Coche();
+		Assertions.assertEquals(0, nuevoCoche.velocidad);
 	}
 
 }
